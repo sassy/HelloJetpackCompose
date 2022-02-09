38eca17e44e8cb447e7a152d6d3dba0fdc3fda1d
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-10 08:42:21 +0900

    insert text

---
+++ app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
@@ -0,0 +1,35 @@
+package io.github.sassy.hello_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.sassy.hello_jetpack_compose.ui.theme.HelloJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello world!")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HelloJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
