6c5f4f4dbdc31924fda7a4deecaa1ef1d6586083
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-11 00:04:35 +0900

    fix layout

--- app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
+++ app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.sassy.hello_jetpack_compose.ui.theme.HelloJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,13 +30,16 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.aiai),
+            contentDescription = "Profile Picture",
+            modifier = Modifier.size(40.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Image(
-                painter = painterResource(id = R.drawable.aiai),
-                contentDescription = "Profile Picture",
-            )
             Text(text = msg.author)
+            Spacer(modifier = Modifier.width(4.dp))
             Text(text = msg.body)
         }
     }
