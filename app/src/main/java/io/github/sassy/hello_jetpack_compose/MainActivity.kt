1db692faff08b2859bba78db493447da84eb6ce7
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-10 23:59:08 +0900

    add Image

--- app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
+++ app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
@@ -3,13 +3,16 @@ package io.github.sassy.hello_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.sassy.hello_jetpack_compose.ui.theme.HelloJetpackComposeTheme
 
@@ -26,9 +29,15 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.aiai),
+                contentDescription = "Profile Picture",
+            )
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
