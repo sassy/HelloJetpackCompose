df9cad225e08047fa2f719ef2f2d2f334c01c66d
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-10 23:47:07 +0900

    add Column

--- app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
+++ app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.sassy.hello_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,18 +17,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    MessageCard(msg = Message("Satoshi", "Hey, take a look at Jetpack Compose, it's great!"))
 }
