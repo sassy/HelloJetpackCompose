ba3959a895746827416be1a1d17c51a0fdbb178e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-11 01:44:19 +0900

    apply Material Theme.

--- app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
+++ app/src/main/java/io/github/sassy/hello_jetpack_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -21,7 +22,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            HelloJetpackComposeTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
         }
     }
 }
@@ -34,13 +37,26 @@ fun MessageCard(msg: Message) {
         Image(
             painter = painterResource(id = R.drawable.aiai),
             contentDescription = "Profile Picture",
-            modifier = Modifier.size(40.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2,
+            )
             Spacer(modifier = Modifier.width(4.dp))
-            Text(text = msg.body)
+            Surface(shape=MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                )
+            }
         }
     }
 }
@@ -48,5 +64,7 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Satoshi", "Hey, take a look at Jetpack Compose, it's great!"))
+    HelloJetpackComposeTheme {
+        MessageCard(msg = Message("Satoshi", "Hey, take a look at Jetpack Compose, it's great!"))
+    }
 }
